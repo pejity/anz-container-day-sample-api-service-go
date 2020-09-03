b33f32db7a7ef02fefc10d785380e9b8b088f273
pejity <70702275+pejity@users.noreply.github.com>
2020-09-03 16:03:43 +0300

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Alex"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
